3531a7d6fb40f3bb1f7d003080bcf5462aaf50ef
Mi3i4 <zelmafia@gmail.com>
2024-11-24 11:17:09 +0300

    4_validate_update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :interests
-  has_and_belongs_to_many :skills, class_name: 'Skill'
+  has_and_belongs_to_many :skills
 
   validates :name, presence: true
   validates :surname, presence: true
+  validates :patronymic, presence: true
+  validates :nationality, presence: true
+  validates :country, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :age, presence: true, numericality: { greater_than: 0, less_than: 90 }
+  validates :age, presence: true, numericality: { greater_than: 0, less_than: 91 }
   validates :gender, presence: true, inclusion: { in: ['male', 'female'] }
 end
