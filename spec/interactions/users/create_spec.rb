a6692f214dea722306ef29b513fffb6cbd2e451b
Mi3i4 <zelmafia@gmail.com>
2024-11-25 08:52:52 +0300

    6_ruby_3_way

--- spec/interactions/users/create_spec.rb
+++ spec/interactions/users/create_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Users::Create, type: :interaction do
   }
 
   it 'creates a user successfully with valid params' do
-    result = described_class.run(user_params: user_params)
+    result = described_class.run(user_params:)
     expect(result.valid?).to be_truthy
     expect(result.result).to be_a(User)
   end
@@ -30,36 +30,36 @@ RSpec.describe Users::Create, type: :interaction do
   end
 
   it 'associates interests and skills' do
-    result = described_class.run(user_params: user_params)
+    result = described_class.run(user_params:)
     user = result.result
     expect(user.interests.count).to eq(3)
     expect(user.skills.count).to eq(2)
   end
 
   it 'creates valid full_name' do
-    result = described_class.run(user_params: user_params)
+    result = described_class.run(user_params:)
     user = result.result
     expect(user.full_name).to eq('Coder Alex Some')
   end
 
   it 'fails if email already exists' do
     User.create(user_params.except(:skills, :interests))
-    result = described_class.run(user_params: user_params)
+    result = described_class.run(user_params:)
     expect(result.valid?).to be_falsey
     expect(result.errors.full_messages).to include('Email is already taken')
   end
 
   it 'fails if age is not within the valid range' do
-    invalid_params = user_params.except(:skills, :interests).merge( { age: 91 } )
+    invalid_params = user_params.merge( { age: 91 } )
     result = described_class.run(user_params: invalid_params)
     expect(result.valid?).to be_falsey
     expect(result.errors.full_messages).to include('Age is not in (1..90) range')
   end
 
   it 'fails if gender is not male or female' do
-    invalid_params = user_params.except(:skills, :interests).merge( { gender: 'dog' } )
+    invalid_params = user_params.merge( { gender: 'dog' } )
     result = described_class.run(user_params: invalid_params)
     expect(result.valid?).to be_falsey
-    expect(result.errors.full_messages).to include('Gender is should be male or female')
+    expect(result.errors.full_messages).to include('Gender should be male or female')
   end
 end
