4bba886b99181dc345009ec6af1c3613745d3243
Mi3i4 <zelmafia@gmail.com>
2024-11-25 08:52:52 +0300

    6_ruby_3_way

--- app/interactions/users/create.rb
+++ app/interactions/users/create.rb
@@ -1,5 +1,4 @@
 class Users::Create < ActiveInteraction::Base
-  set_callback :filter, :before, -> { create_full_name }
 
   hash :user_params do
     string :name
@@ -21,7 +20,7 @@ class Users::Create < ActiveInteraction::Base
     return nil if errors.any?
 
     User.transaction do
-      user = User.create(user_params.except(:skills, :interests))
+      user = User.create(user_params.except(:skills, :interests).merge(full_name:))
       create_interests(user)
       create_skills(user)
       user
@@ -30,28 +29,28 @@ class Users::Create < ActiveInteraction::Base
 
   private
 
-  def create_full_name
-    user_params[:full_name] = user_params[:full_name] ||
-      [user_params[:surname], user_params[:name], user_params[:patronymic].presence].compact.join(' ')
+  def full_name
+    user_params[:full_name] ||
+      [user_params[:surname], user_params[:name], user_params[:patronymic]].compact.join(' ')
   end
 
   def validate_user_params
     errors.add(:email, 'is already taken') if User.exists?(email: user_params[:email])
     errors.add(:age, 'is not in (1..90) range') unless (1..90).include?(user_params[:age])
-    errors.add(:gender, 'is should be male or female') unless %w[male female].include?(user_params[:gender])
+    errors.add(:gender, 'should be male or female') unless %w[male female].include?(user_params[:gender])
   end
 
   def create_interests(user)
     interest_objects = Interest.where(name: user_params[:interests]).to_a
     new_interests = user_params[:interests] - interest_objects.map(&:name)
-    new_interests.each { |name| interest_objects << Interest.create!(name: name) }
+    new_interests.each { |name| interest_objects << Interest.create!(name:) }
     user.interests = interest_objects
   end
 
   def create_skills(user)
     skill_objects = Skill.where(name: user_params[:skills]).to_a
     new_skills = user_params[:skills] - skill_objects.map(&:name)
-    new_skills.each { |name| skill_objects << Skill.create!(name: name) }
+    new_skills.each { |name| skill_objects << Skill.create!(name:) }
     user.skills = skill_objects
   end
 end
